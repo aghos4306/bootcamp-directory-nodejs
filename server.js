2ee173ab171ad7a341ac939dbc4d41b5a6480a6c
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-03-18 11:40:00 +0200

    logger middleware done manually

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 //Routes files
 const bootcamps = require('./routes/bootcamps');
 
@@ -8,9 +9,8 @@ dotenv.config({ path: './config/config.env' });
 const app = express();
 
 const logger = (req, res, next) => {
-    req.hello = 'Hello World'
-    console.log('Middleware ran')
-    next()
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`)
+    next() 
 }
 
 app.use(logger)
