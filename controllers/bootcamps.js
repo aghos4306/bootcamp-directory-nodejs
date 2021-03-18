b0cad93b339ef393914cfa0775025bf4aa8470e9
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-03-18 16:11:59 +0200

    get single bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,28 @@ const Bootcamp = require('../models/Bootcamp')
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
 // @access      Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'get all bootcamps from database', hello: req.hello })
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find()
+        res.status(200).json({ success: true, data: bootcamps })
+    } catch (err) {
+        res.status(400).json({ success: false })
+    }
 }
 
 // @desc        Get single bootcamp
 // @route       GET /api/v1/bootcamps/:id
 // @access      Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `get single bootcamp with id ${req.params.id}` })
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id)
+        res.status(200).json({ success: true, data: bootcamp })
+        if(!bootcamp) {
+           return res.status(400).json({ success: false })
+        }
+    } catch (err) {
+        res.status(400).json({ success: false })
+    }
 }
 
 // @desc        Create new bootcamp
