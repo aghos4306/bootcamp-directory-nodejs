499bcfd7deb5da6db40cd1c4d5bb6f397e547187
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-04-05 08:28:54 +0300

    mongoose pre middleware and slugify

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,11 +1,12 @@
 const Bootcamp = require('../models/Bootcamp')
 const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/async')
 
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
 // @access      Public
-exports.getBootcamps = async (req, res, next) => {
-    try {
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+    
         const bootcamps = await Bootcamp.find()
 
         if(!bootcamps) {
@@ -13,11 +14,8 @@ exports.getBootcamps = async (req, res, next) => {
             return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
         } 
         res.status(200).json({ success: true, data: bootcamps })  
-    } catch (err) {
-        //res.status(400).json({ success: false })
-        next(err)
-    }
-}
+    
+})
 
 // @desc        Get single bootcamp
 // @route       GET /api/v1/bootcamps/:id
