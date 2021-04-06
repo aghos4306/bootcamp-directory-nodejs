8acfa5f0ee44836811ea515bb2860e851cf3c3ec
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-04-06 10:07:33 +0300

    server side pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,17 +9,73 @@ const Bootcamp = require('../models/Bootcamp')
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
        
         let query;
-        let queryStr = JSON.stringify(req.query);
+
+        //copy req.query
+        const reqQuery = { ...req.query }
+
+        //fields to exclude
+        const removeFields = ['select', 'sort', 'page', 'limit']
+
+        // loop over removeFields and delete them from reqQuery
+        removeFields.forEach(param => delete reqQuery[param])
+
+        //create query string
+        let queryStr = JSON.stringify(reqQuery);
+
+        //create operators ($gt, $gte, $lt etc)
         queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+        //finding resource
         query = Bootcamp.find(JSON.parse(queryStr))
 
+        // select fields
+        if(req.query.select) {
+            const fields = req.query.select.split(',').join(' ') //turn to array and spaced out string
+            query = query.select(fields)
+        }
+
+        // sort
+        if(req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ')
+            query = query.sort(sortBy)
+        } else {
+            query = query.sort('-createdAt')
+        }
+
+        // pagination
+        const page = parseInt(req.query.page, 10) || 1
+        const limit = parseInt(req.query.limit, 10) || 25
+        const startIndex = (page - 1) * limit
+        const endIndex = page * limit
+        const total = await Bootcamp.countDocuments()
+
+        query = query.skip(startIndex).limit(limit) 
+
+        //execute query
         const bootcamps = await query
+
+        // pagination result
+        const pagination = {}
+
+        if(endIndex < total) {
+            pagination.next = {
+                page: page + 1,
+                limit:limit
+            }
+        }
+
+        if(startIndex > 0) {
+            pagination.prev = {
+                page: page - 1,
+                limit
+            }
+        }
 
         if(!bootcamps) {
             //res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
             return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
         } 
-        res.status(200).json({ success: true, count:bootcamps.length, data: bootcamps })  
+        res.status(200).json({ success: true, count:bootcamps.length, pagination, data: bootcamps })  
     
 })
 
