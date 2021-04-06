92ad10df299e65f47a0529aab0d51ed64ecff979
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-04-06 13:41:46 +0300

    course model and course seeder

--- seeder.js
+++ seeder.js
@@ -7,6 +7,7 @@ dotenv.config({ path: './config/config.env' })
 
 //load models
 const Bootcamp = require('./models/Bootcamp')
+const Course = require('./models/Course')
 
 // connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -18,11 +19,13 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'))
 
 // import data into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps)
+        await Course.create(courses)
         console.log('Data imported...')
         process.exit()
     } catch (err) {
@@ -34,6 +37,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany()
+        await Course.deleteMany()
         console.log('Data deleted...')
         process.exit()
     } catch (err) {
